40a2f03da172f93a3583382bcbe19771122d74d8
Łukasz Migut <real.d3us@gmail.com>
2025-04-09 13:22:24 +0200

    Lint with fix all the files

--- app/registration/page.tsx
+++ app/registration/page.tsx
@@ -1,8 +1,8 @@
 export default function RegistrationPage() {
-    return (
-        <div>
-        <h1>Registration Page</h1>
-        <p>This is the registration page.</p>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Registration Page</h1>
+      <p>This is the registration page.</p>
+    </div>
+  );
 }
