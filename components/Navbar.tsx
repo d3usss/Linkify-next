f7256528326166b501ed2d89b639f036fcab0466
Łukasz Migut <real.d3us@gmail.com>
2025-04-19 13:57:40 +0200

    Add preline components: Navbar, theme-switcher

---
+++ components/Navbar.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import Link from "next/link";
+import { ThemeSwitcher } from "./ThemeSwitcher";
+
+export function Navbar() {
+  return (
+    <header className="relative flex flex-wrap sm:justify-start items-center sm:flex-nowrap w-full bg-neutral-200 text-sm dark:bg-neutral-800 py-2 rounded-xl">
+      <nav className="max-w-[85rem] w-full mx-auto px-4 sm:flex sm:items-center sm:justify-between">
+        <div className="flex items-center justify-between">
+          <Link
+            className="font-semibold focus:outline-hidden hover:opacity-80 inline-flex items-center gap-x-2 text-xl text-white"
+            href="/"
+            aria-label="Brand"
+          >
+            <Image
+              src="/linkify.jpg"
+              alt="Linkify"
+              width={40}
+              height={40}
+              className="rounded-full"
+            />
+            Linkify
+          </Link>
+
+          <div className="sm:hidden">
+            <button
+              type="button"
+              className="hs-collapse-toggle relative size-9 flex justify-center items-center gap-x-2 rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 focus:outline-hidden focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-transparent dark:border-neutral-700 dark:text-white dark:hover:bg-white/10 dark:focus:bg-white/10"
+              id="hs-navbar-example-collapse"
+              aria-expanded="false"
+              aria-controls="hs-navbar-example"
+              aria-label="Toggle navigation"
+              data-hs-collapse="#hs-navbar-example"
+            >
+              <svg
+                className="hs-collapse-open:hidden shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="3" x2="21" y1="6" y2="6" />
+                <line x1="3" x2="21" y1="12" y2="12" />
+                <line x1="3" x2="21" y1="18" y2="18" />
+              </svg>
+              <svg
+                className="hs-collapse-open:block hidden shrink-0 size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+              <span className="sr-only">Toggle navigation</span>
+            </button>
+          </div>
+        </div>
+        <div
+          id="hs-navbar-example"
+          className="hidden hs-collapse overflow-hidden transition-all duration-300 basis-full grow sm:block"
+          aria-labelledby="hs-navbar-example-collapse"
+        >
+          <div className="flex flex-col gap-5 mt-5 sm:flex-row sm:items-center sm:justify-end sm:mt-0 sm:ps-5">
+            <Link
+              className="font-medium text-blue-500 focus:outline-hidden"
+              href="/"
+              aria-current="page"
+            >
+              Home
+            </Link>
+            <Link
+              className="font-medium text-gray-600 hover:text-gray-400 focus:outline-hidden focus:text-gray-400 dark:text-neutral-400 dark:hover:text-neutral-500 dark:focus:text-neutral-500"
+              href="/dashboard"
+            >
+              Dashboard
+            </Link>
+            <ThemeSwitcher />
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
